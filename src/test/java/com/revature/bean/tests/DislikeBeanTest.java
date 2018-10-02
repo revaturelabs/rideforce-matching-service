be79882ec296ae3f6ea04e23b99f924bd737fb34
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-10-02 13:10:46 -0400

    creating structure for junit testing

---
+++ src/test/java/com/revature/bean/tests/DislikeBeanTest.java
@@ -0,0 +1,14 @@
+package com.revature.bean.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DislikeBeanTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
