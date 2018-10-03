5b310478bbe895efaac54c5d37d4caf66baa33f8
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-10-03 16:29:27 -0400

    rapid changes to testing, commiting often

--- src/test/java/com/revature/service/tests/DislikeServiceIntegrationTest.java
+++ src/test/java/com/revature/service/tests/DislikeServiceIntegrationTest.java
@@ -1,12 +1,17 @@
 package com.revature.service.tests;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -18,31 +23,22 @@ import com.revature.rideshare.matching.repositories.DislikeRepository;
 import com.revature.rideshare.matching.services.DislikeService;
 
 @RunWith(SpringRunner.class)
+@DataJpaTest
 public class DislikeServiceIntegrationTest {
 
-	@TestConfiguration
-	static class DislikeServiceTestContextConfiguration {
-
-		@Bean
-		public DislikeService disService() {
-			return new DislikeService();
-		}
-	}
-
 	@Autowired
-	private DislikeService disService;
-
-	@MockBean
+	private TestEntityManager entityManager;
+	
+	@Autowired
 	private DislikeRepository dislikeRepo;
 
-	@Before
-	public void setUp() {
-	    Dislike dis0 = new Dislike(new Pair(1,2));
-	    Dislike dis1 = new Dislike(new Pair(1,3));
-	    Dislike dis2 = new Dislike(new Pair(1,4));
-	    List<Dislike> dees = new ArrayList<>();
-	 
-	    Mockito.when(dislikeRepo.findByPairUserId(1)).thenReturn(dees);
+	
+	@Test
+	public void shouldBeEmpty() {
+		Iterable<Dislike> dis = dislikeRepo.findAll();
+ 
+		assertThat(dis).isEmpty();
 	}
+ 
 
 }
