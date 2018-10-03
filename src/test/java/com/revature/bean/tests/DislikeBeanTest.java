d1f8c87c357066dccd67e3d32304480546b56104
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-10-03 08:54:12 -0400

    testing for the models!

--- src/test/java/com/revature/bean/tests/DislikeBeanTest.java
+++ src/test/java/com/revature/bean/tests/DislikeBeanTest.java
@@ -1,14 +1,40 @@
 package com.revature.bean.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.revature.rideshare.matching.beans.Dislike;
+import com.revature.rideshare.matching.beans.Pair;
 
 public class DislikeBeanTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDislikeConstructor() {
+
+		Dislike dis = new Dislike(new Pair(100, 200));
+
+		assertSame("Pair constructor accepting two ints did not create obj as expected.", 100,
+				dis.getPair().getUserId());
+		assertSame("Pair constructor accepting two ints did not create obj as expected.", 200,
+				dis.getPair().getAffectedId());
+	}
+
+	public void testDislikeEquals() {
+
+		Pair pair = new Pair(1, 2);
+		Pair pairEq = new Pair(1, 2);
+		Pair pairNotEq = new Pair(3, 4);
+
+		Dislike dis = new Dislike(pair);
+		Dislike disEq = new Dislike(pairEq);
+		Dislike disNotEq = new Dislike(pairNotEq);
+
+		assertTrue("Dislike equals override not functioning properly; should be true.", dis.equals(disEq));
+		assertFalse("Dislike equals override not functioning properly; should be false.", dis.equals(disNotEq));
+
 	}
 
 }
---
+++ src/test/java/com/revature/bean/tests/PairBeanTest.java
@@ -0,0 +1,58 @@
+package com.revature.bean.tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import org.hibernate.validator.HibernateValidator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import com.revature.rideshare.matching.beans.Pair;
+
+public class PairBeanTest {
+	
+private LocalValidatorFactoryBean localValidatorFactory;
+	
+	@Before
+	public void setupValidatorFactory () {
+		localValidatorFactory = new LocalValidatorFactoryBean();
+		localValidatorFactory.setProviderClass(HibernateValidator.class);
+		localValidatorFactory.afterPropertiesSet();
+	}
+
+	@Test
+	public void testConstructor() {
+		
+		Pair pair = new Pair(1, 2);
+		
+		
+		assertSame("Pair constructor accepting two ints did not create obj as expected.", 1, pair.getUserId());
+		assertSame("Pair constructor accepting two ints did not create obj as expected.", 2, pair.getAffectedId());
+	}
+
+	@Test
+	public void testPairEquals() {
+		
+		Pair pair = new Pair(1, 2);
+		Pair pairEq = new Pair(1, 2);
+		Pair pairNotEq = new Pair(3, 4);
+		
+		assertTrue("Pair equals override not functioning properly; should be true.", pair.equals(pairEq));
+		assertFalse("Pair equals override not functioning properly; should be false.", pair.equals(pairNotEq));
+		
+	}
+	
+	@Test
+	public void testGetUserId() {
+		Pair p = new Pair(101,201);
+		assertTrue(p.getUserId() == 101);
+	}
+	
+	@Test
+	public void testGetAffectedId() {
+		Pair p = new Pair(101,201);
+		assertTrue(p.getAffectedId() == 201);
+	}
+}
