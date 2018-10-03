ba65345c6a7f7615314e56bfb198da001fe766ab
davidcalkins <david.calkins.1000@gmail.com>
2018-10-03 17:03:52 -0400

    Added Javadoc to Route

--- src/main/java/com/revature/rideshare/matching/beans/Route.java
+++ src/main/java/com/revature/rideshare/matching/beans/Route.java
@@ -4,33 +4,77 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Route {
+	
+	/** The distance between the source address and the destination address.
+	 * <br><br>
+	 * The current implementation according to 
+	 * the {@code RouteService} in the {@code map-service} project defines 
+	 * this in meters.  */
 	long distance;
+	
+	/** The duration of the drive, according to google maps, between the source
+	 * address and destination address. 
+	 * <br><br>
+	 * The current implementation according to 
+	 * the {@code RouteService} in the {@code map-service} project defines 
+	 * this in seconds. */
 	long duration;
-
+	
+	/**  
+	 * Constructs a new Route object with all of it's parameters uninitialized.
+	 */
 	public Route() {
 	}
-
+	
+	/**  
+	 * Constructs a new Route object. 
+	 * 
+	 * @param distance - The distance between the source address and destination
+	 * 					address.
+	 * @param duration - The duration of the drive between the source address 
+	 * 					and destination address.
+	 */
 	public Route(long distance, long duration) {
 		this.distance = distance;
 		this.duration = duration;
 	}
 
+	/**
+	 * Returns the distance between the source address and destination address 
+	 * as computed by google maps.
+	 * @return distance between addresses
+	 */
 	public long getDistance() {
 		return distance;
 	}
 
+	/**
+	 * Sets the distance between the source address and destination address.
+	 * @param distance between addresses
+	 */
 	public void setDistance(long distance) {
 		this.distance = distance;
 	}
 
+	/**
+	 * Returns the duration of the drive between the source address and 
+	 * destination address as computed by google maps.
+	 * @return duration of drive between addresses
+	 */
 	public long getDuration() {
 		return duration;
 	}
 
+	/**
+	 * Sets the duration of the drive between the source address and destination 
+	 * address.
+	 * @param duration of drive between addresses
+	 */
 	public void setDuration(long duration) {
 		this.duration = duration;
 	}
 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -40,6 +84,7 @@ public class Route {
 		return result;
 	}
 
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
