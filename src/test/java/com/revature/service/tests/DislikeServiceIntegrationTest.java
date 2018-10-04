e097cc7a25fd33692faf9c8698fea4b309597024
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-10-04 10:43:01 -0400

    Dislike service test functional

--- src/test/java/com/revature/service/tests/DislikeServiceIntegrationTest.java
+++ src/test/java/com/revature/service/tests/DislikeServiceIntegrationTest.java
@@ -1,29 +1,27 @@
 package com.revature.service.tests;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.rideshare.matching.Application;
 import com.revature.rideshare.matching.beans.Dislike;
-import com.revature.rideshare.matching.beans.Pair;
 import com.revature.rideshare.matching.repositories.DislikeRepository;
-import com.revature.rideshare.matching.services.DislikeService;
 
+@SpringBootTest(classes = Application.class)
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace= Replace.NONE)
 public class DislikeServiceIntegrationTest {
 
 	@Autowired
@@ -35,9 +33,8 @@ public class DislikeServiceIntegrationTest {
 	
 	@Test
 	public void shouldBeEmpty() {
-		Iterable<Dislike> dis = dislikeRepo.findAll();
- 
-		assertThat(dis).isEmpty();
+		List<Dislike> dis = dislikeRepo.findAll();
+		Assertions.assertThat(dis).isEmpty();
 	}
  
 
