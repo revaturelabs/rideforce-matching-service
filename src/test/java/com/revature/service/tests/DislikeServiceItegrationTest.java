81fe1d934a24e8630a7c5f0c9da261f179312f4f
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-10-05 15:10:55 -0400

    removed unused import

--- src/test/java/com/revature/service/tests/DislikeServiceItegrationTest.java
+++ src/test/java/com/revature/service/tests/DislikeServiceItegrationTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
