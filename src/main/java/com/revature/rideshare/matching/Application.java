af5ec41853741f91ca5a0aa4b6b2d72448d033c3
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-10-10 14:59:46 -0400

    Logging configuration needed modification.  It did not work as I thought it would

--- src/main/java/com/revature/rideshare/matching/Application.java
+++ src/main/java/com/revature/rideshare/matching/Application.java
@@ -1,5 +1,7 @@
 package com.revature.rideshare.matching;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,13 +9,19 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.revature.rideshare.matching.controllers.MatchingController;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaRepositories
-@EnableCircuitBreaker
 public class Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) throws Exception {
+		LOGGER.trace("Trace Testing logging");
+		LOGGER.debug("Debug Testing Logging");
+		LOGGER.error("Error Testing Logging");
+		LOGGER.warn(" Warn Testing Logging");
 		SpringApplication.run(Application.class, args);
 	}
 }
--- src/main/java/com/revature/rideshare/matching/controllers/MatchingController.java
+++ src/main/java/com/revature/rideshare/matching/controllers/MatchingController.java
@@ -3,6 +3,8 @@ package com.revature.rideshare.matching.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,94 +22,131 @@ import com.revature.rideshare.matching.services.MatchService;
 @RestController
 @RequestMapping("matches")
 public class MatchingController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MatchingController.class);
+	private static final String MSG = "Get request made to matching controller. UserId : %d passed. userClient called to find user by that id. userClient returned the user: %d";
+	private static final String NULL = "userClient return a null user object.";
 	@Autowired
 	UserClient userClient;
 
 	@Autowired
 	MatchService matchService;
-	
+
 	@Autowired
 	LikeService likeService;
-	
+
 	@Autowired
 	DislikeService dislikeService;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getAll(@PathVariable int id) {
 		User rider = userClient.findById(id);
+		if (rider == null) {
+			LOGGER.trace(NULL);
+		} else {
+			LOGGER.trace(MSG, id, rider.getFirstName());
+		}
 		return matchService.findMatches(rider).stream()
 				.map(driver -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(driver.getId()).toString())
 				.collect(Collectors.toList());
 	}
-	
+
 	@RequestMapping(value = "/likes-dislikes/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getAllMinusAffects(@PathVariable int id) {
+		
 		User rider = userClient.findById(id);
+		if (rider == null) {
+			LOGGER.trace(NULL);
+		} else {
+			LOGGER.trace(MSG, id, rider.getFirstName());
+		}
 		return matchService.findMatchesByAffects(rider).stream()
 				.map(driver -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(driver.getId()).toString())
 				.collect(Collectors.toList());
 	}
-	
+
 	@RequestMapping(value = "/distance/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getByDistance(@PathVariable int id) {
 		User rider = userClient.findById(id);
+		if (rider == null) {
+			LOGGER.trace(NULL);
+		} else {
+			LOGGER.trace(MSG, id, rider.getFirstName());
+		}
 		return matchService.findMatchesByDistance(rider).stream()
 				.map(driver -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(driver.getId()).toString())
 				.collect(Collectors.toList());
 
 	}
-	
+
 	@RequestMapping(value = "/batch-end/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<String> getByBatchEnd(@PathVariable int id){
-        User rider = userClient.findById(id);
-        return matchService.findMatchesByBatchEnd(rider).stream()
-                .map(driver -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(driver.getId()).toString())
-                .collect(Collectors.toList());
-    }
+	public List<String> getByBatchEnd(@PathVariable int id) {
+		User rider = userClient.findById(id);
+		if (rider == null) {
+			LOGGER.trace(NULL);
+		} else {
+			LOGGER.trace(MSG, id, rider.getFirstName());
+		}
+		return matchService.findMatchesByBatchEnd(rider).stream()
+				.map(driver -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(driver.getId()).toString())
+				.collect(Collectors.toList());
+	}
 
 	@RequestMapping(value = "/likes/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getLiked(@PathVariable("id") int id) {
 		List<String> likes = null;
-		//TODO: Uncomment to test with real users!
-		likes = likeService.getLikes(id).stream()
-				.map(like -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(like.getPair().getAffectedId()).toString())
-				.collect(Collectors.toList());
-//		likes = likeService.getLikes(id).stream()
-//				.map(like -> like.toString())
-//				.collect(Collectors.toList());
+
+		likes = likeService.getLikes(id).stream().map(like -> UriComponentsBuilder.fromPath("/users/{id}")
+				.buildAndExpand(like.getPair().getAffectedId()).toString()).collect(Collectors.toList());
+
+		if (likes.isEmpty()) {
+			LOGGER.trace("Mapping process did not return any URIs associated with this user id: %d", id);
+		} else {
+			LOGGER.trace("likeService.getLikes called with id: %d which is then mapped to create a list of uri's that contain a path to "
+					+ "to get the users (drivers) that they have liked.", id);
+		}
+		
 		return likes;
 	}
 
 	@RequestMapping(value = "/likes/{id}/{liked}", method = RequestMethod.PUT)
 	public void addLiked(@PathVariable("id") int id, @PathVariable("liked") int liked) {
-		likeService.saveLike(id,liked);
+		LOGGER.trace("Like service called to save a like for the userId %d and affected userId %d.", id, liked);
+		likeService.saveLike(id, liked);
 	}
 
 	@RequestMapping(value = "/likes/{id}/{liked}", method = RequestMethod.DELETE)
 	public void deleteLiked(@PathVariable("id") int id, @PathVariable("liked") int liked) {
-		likeService.deleteLike(id,liked);
+		LOGGER.trace("Like service called to delete a like for the userId %d and affected userId %d.", id, liked);
+		likeService.deleteLike(id, liked);
 	}
 
 	@RequestMapping(value = "dislikes/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getDisliked(@PathVariable("id") int id) {
 		List<String> dislikes = null;
-		//TODO: Uncomment to test with real users!
-		dislikes = dislikeService.getDislikes(id).stream()
-				.map(dislike -> UriComponentsBuilder.fromPath("/users/{id}").buildAndExpand(dislike.getPair().getAffectedId()).toString())
-				.collect(Collectors.toList());
-//		dislikes = dislikeService.getDislikes(id).stream()
-//				.map(dislike -> dislike.toString())
-//				.collect(Collectors.toList());
+		
+		dislikes = dislikeService.getDislikes(id).stream().map(dislike -> UriComponentsBuilder.fromPath("/users/{id}")
+				.buildAndExpand(dislike.getPair().getAffectedId()).toString()).collect(Collectors.toList());
+
+		if (dislikes.isEmpty()) {
+			LOGGER.trace("Mapping process did not return any URIs associated with this user id: %d ", id);
+		} else {
+			LOGGER.trace("dislikeService.getDislikes called with id: %d which was then mapped to create a list of uri's that contain a path to "
+					+ "to get the users (drivers) that they have liked.", id);
+		}
+		
 		return dislikes;
 	}
 
 	@RequestMapping(value = "/dislikes/{id}/{disliked}", method = RequestMethod.PUT)
 	public void addDisliked(@PathVariable("id") int id, @PathVariable("disliked") int disliked) {
 		dislikeService.saveDislike(id, disliked);
+		LOGGER.trace("Dislike service called to save a dislike for the userId %d and affected userId %d.", id, disliked);
 	}
 
 	@RequestMapping(value = "/dislikes/{id}/{disliked}", method = RequestMethod.DELETE)
 	public void deletedisLiked(@PathVariable("id") int id, @PathVariable("disliked") int disliked) {
+		
 		dislikeService.deleteDislike(id, disliked);
+		LOGGER.trace("Dislike service called to delete a dislike for the userId %d and affected userId %d.", id, disliked);
 	}
 }
