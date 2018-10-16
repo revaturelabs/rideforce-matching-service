f541c96a338f0946c27edc5505a7ed9162f4df52
davidcalkins <david.calkins.1000@gmail.com>
2018-10-16 15:15:41 -0400

    Fallback now throws meaningful exceptions

--- src/main/java/com/revature/rideshare/matching/clients/UserClientFallback.java
+++ src/main/java/com/revature/rideshare/matching/clients/UserClientFallback.java
@@ -1,10 +1,12 @@
 package com.revature.rideshare.matching.clients;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 import com.revature.rideshare.matching.beans.User;
+import com.revature.rideshare.matching.exceptions.FeignClientFallbackException;
 
 /** This class is the fallback implementation for when a service cannot 
  * make a successful connection with a UserClient. The general behavior
@@ -15,12 +17,16 @@ public class UserClientFallback implements UserClient {
 
 	@Override
 	public List<User> findByOfficeAndRole(int officeId, String role) {
-		return null;
+		// Note, a different, but similar exception is actually thrown if the 
+		// fallback throws an exception. 
+		throw new FeignClientFallbackException("Counldn't connect to the UserClient");
 	}
 
 	@Override
 	public User findById(int id) {
-		return null;
+		// Note, a different, but similar exception is actually thrown if the 
+		// fallback throws an exception. 
+		throw new FeignClientFallbackException("Counldn't connect to the UserClient");
 	}
 
 
