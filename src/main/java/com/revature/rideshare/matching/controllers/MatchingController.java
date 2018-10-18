07fef3750ecd485827cb39948f204f022af39d08
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2018-10-18 10:44:48 -0400

    Temporary bug fix

--- src/main/java/com/revature/rideshare/matching/controllers/MatchingController.java
+++ src/main/java/com/revature/rideshare/matching/controllers/MatchingController.java
@@ -10,11 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +31,7 @@ import com.revature.rideshare.matching.services.MatchService;
 /**
  * The Class MatchingController.
  */
+@CrossOrigin
 @RestController
 @RequestMapping("matches")
 public class MatchingController {
