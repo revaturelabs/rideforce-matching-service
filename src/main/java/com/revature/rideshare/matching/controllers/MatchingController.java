c6e40fdf0eca318d884c1657d99aec478327f224
dehkrueger <drrueger@wpi.edu>
2019-02-03 14:39:48 -0500

    Added CORS from localhost:4200 to matching controller

--- src/main/java/com/revature/rideshare/matching/controllers/MatchingController.java
+++ src/main/java/com/revature/rideshare/matching/controllers/MatchingController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +34,7 @@ import com.revature.rideshare.matching.services.MatchService;
  * The Class MatchingController.
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("matches")
 public class MatchingController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MatchingController.class);
